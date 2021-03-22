d8665d53abe2d7d8ea2a46690d24a71723e56226
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-03-22 13:03:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+
+namespace SQLPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

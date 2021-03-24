e1535f96a9b3d14f80c6c2b3caa302e3f0876caa
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-03-24 15:19:43 -0500

    Dense Rank Usage

---
+++ Scripts/DENSE_RANK.sql
@@ -0,0 +1,17 @@
+﻿
+
+-- Get total number of orders by each customers including those who didn't make any order
+SELECT Customers.NAME, COUNT(Orders.ORDERID) as 'Total Order' FROM CUSTOMERS
+	LEFT JOIN Orders ON Customers.CUSTOMERID = Orders.CUSTOMERID
+	GROUP BY Customers.NAME
+	ORDER BY [Total Order] DESC
+
+
+--Lets give a rank to customers based on the number of orders they make
+--1. Count total orders using Join and use it as a subquery (Remember to give alias here: Ranks)
+--2. Use DENSE_RANK 
+SELECT Ranks.Name, DENSE_RANK() OVER (ORDER BY Ranks.[Total Orders] DESC) AS 'Rank'
+	FROM 
+		(SELECT Customers.NAME as [Name], COUNT(Orders.ORDERID) as 'Total Orders' FROM CUSTOMERS
+		LEFT JOIN Orders ON Customers.CUSTOMERID = Orders.CUSTOMERID
+		GROUP BY Customers.NAME) as Ranks

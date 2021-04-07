2bd23f66be52ebe8cb763817b15522f677fb23ec
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-06 19:59:21 -0500

    Update Right Join

--- Scripts/RIGHT_JOIN.sql
+++ Scripts/RIGHT_JOIN.sql
@@ -1,5 +1,5 @@
 ﻿-- Get total number of orders by each customers including those who didn't make any order
 SELECT Customers.NAME, COUNT(Orders.ORDERID) as 'Total Order' FROM Orders
-	LEFT JOIN Customers ON Customers.CUSTOMERID = Orders.CUSTOMERID
+	RIGHT JOIN Customers ON Customers.CUSTOMERID = Orders.CUSTOMERID
 	GROUP BY Customers.NAME
 	ORDER BY [Total Order] DESC
